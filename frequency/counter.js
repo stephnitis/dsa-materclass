32cfdaaeec76f813187798dbbbc789c94e05793e
Stephanie Hill <stephni.hill@gmail.com>
2023-01-08 15:01:26 -0800

    refactor example

--- frequency/counter.js
+++ frequency/counter.js
@@ -17,7 +17,7 @@ function same(arr1, arr2) {
     if(correctIndex === -1) {
       return false;
     }
-    console.log(arr2)
+    console.log('array two as we splice ---->', arr2)
     // splice removes index from the array so we don't re-use values
     arr2.splice(correctIndex, 1)
   }
@@ -26,4 +26,38 @@ function same(arr1, arr2) {
 
 const array1 = [2, 3, 3, 5]
 const array2 = [4, 9, 9, 25]
-same(array1, array2)
+console.log('result for same function ---->', same(array1, array2));
+
+// refactored
+
+function sameRefactor(arr1, arr2) {
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+  // each object holds the counts of frequency of values in the array
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  // loop over each array, adding 1 to the key when it occurs
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+  }
+  // loop over the objects and see if the occurrences match
+  for (let key in frequencyCounter1) {
+    if(!(key ** 2 in frequencyCounter2)){
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+      return false;
+    }
+  }
+  console.log('frequency counter one ----->', frequencyCounter1);
+  console.log('frequency counter two ----->', frequencyCounter2);
+  return true;
+}
+
+const array3 = [2, 3, 3, 1]
+const array4 = [4, 9, 9, 1]
+console.log('result same refactor function ---->', sameRefactor(array3, array4));
