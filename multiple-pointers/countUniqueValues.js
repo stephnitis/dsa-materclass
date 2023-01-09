e928e361dd5cdb661ac9f3123987cdf06722a225
Stephanie Hill <stephni.hill@gmail.com>
2023-01-08 17:48:12 -0800

    added edge case

--- multiple-pointers/countUniqueValues.js
+++ multiple-pointers/countUniqueValues.js
@@ -2,13 +2,16 @@
 
 // implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted
 
-function countUniqueValues(arr){
+function countUniqueValues(arr) {
   let slow = 0;
   let fast = 1;
-  while(fast < arr.length){
-    if (arr[slow] === arr[fast]){
+  if (arr.length === 0) {
+    return slow;
+  }
+  while (fast < arr.length) {
+    if (arr[slow] === arr[fast]) {
       fast += 1
-    } else if(arr[slow] !== arr[fast]){
+    } else if (arr[slow] !== arr[fast]) {
       slow += 1
       arr[slow] = arr[fast]
     }
@@ -17,4 +20,5 @@ function countUniqueValues(arr){
 }
 
 const array1 = [1, 1, 2, 2, 3, 5, 7, 7, 7, 8];
-console.log('my solution ----->', countUniqueValues(array1))
+const array2 = [1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13];
+console.log('my solution ----->', countUniqueValues(array2))
